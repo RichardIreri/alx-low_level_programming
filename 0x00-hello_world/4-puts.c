351c28c654d2904a60234c2f78ef1b9f5fc0bc9c
RichardIreri <richardmuthoni34@gmail.com>
2022-07-05 00:54:00 +0300

    Displaying output using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main
-{
-	puts("programming is like building a multilingual puzzle");
-}
 
-return 0;
+/* This program prints exactly the string 
+ * "programming is like building a multilingual puzzle
+ * followed by a new line."
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
