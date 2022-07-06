8ba4981d75f5e694b2dee2d512383922f5eb173b
RichardIreri <richardmuthoni34@gmail.com>
2022-07-06 22:49:13 +0300

    Printing  characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * main - Entry point.
+ * Description: write a program that prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success.)
+ */
 int main()
 {
-	putchar("_putchar");
-	
-	return (0);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
+	return (0);
 }
